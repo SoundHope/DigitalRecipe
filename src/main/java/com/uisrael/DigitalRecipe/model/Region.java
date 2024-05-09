7b0006501f875e971d28f6b335dfb8e4d46dbffa
Diego032396 <diegojosuegalvez@gmail.com>
2024-05-08 20:40:21 -0500

    Atributos en las entidades tienda y region

--- src/main/java/com/uisrael/DigitalRecipe/model/Region.java
+++ src/main/java/com/uisrael/DigitalRecipe/model/Region.java
@@ -1,5 +1,23 @@
 package com.uisrael.DigitalRecipe.model;
 
-public class Region {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Region {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String region;
+	
 }
--- src/main/java/com/uisrael/DigitalRecipe/model/Store.java
+++ src/main/java/com/uisrael/DigitalRecipe/model/Store.java
@@ -1,5 +1,27 @@
 package com.uisrael.DigitalRecipe.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Store {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String nombre;
+	private String direccion;
+	private String telefono;
+	
+	
 
 }
