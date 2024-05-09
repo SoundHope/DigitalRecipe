e264bbd2dd4d4538b62335cab22e6912aa393252
Diana <nabeles@outlook.es>
2024-05-08 20:39:46 -0500

    Atributos de las entidades receta e ingredientes

--- src/main/java/com/uisrael/DigitalRecipe/model/Recipe.java
+++ src/main/java/com/uisrael/DigitalRecipe/model/Recipe.java
@@ -1,5 +1,17 @@
 package com.uisrael.DigitalRecipe.model;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
 public class Recipe {
+	
+	private int id;
+	private String nombreReceta;
+	private String descripcionReceta;
+	private String tiempoPreparacion;
 
 }
--- src/main/java/com/uisrael/DigitalRecipe/model/Store.java
+++ src/main/java/com/uisrael/DigitalRecipe/model/Store.java
@@ -1,5 +1,12 @@
 package com.uisrael.DigitalRecipe.model;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString
 public class Store {
 	
 	private int id;
